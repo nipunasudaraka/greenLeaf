1543aa55bd3117cf3cc51e3829961fbaec263c18
Nipuna Amarasooriya <sudarakaeng@gmail.com>
2025-08-14 20:06:10 +0530

    fix errors

--- src/components/about/CommitmentCard.tsx
+++ src/components/about/CommitmentCard.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-function CommitmentCard({ title, Icon }) {
+function CommitmentCard({
+  title,
+  Icon,
+}: {
+  title: string;
+  Icon: React.ElementType;
+}) {
   return (
     <div className="flex flex-col items-center justify-center w-28 lg:w-32 aspect-square p-2 bg-white rounded-lg shadow-md">
       <Icon className="text-2xl text-secondary-color" />
